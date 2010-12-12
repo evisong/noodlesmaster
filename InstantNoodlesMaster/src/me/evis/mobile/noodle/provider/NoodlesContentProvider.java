cc8896a44e5379e40e333325101538eaf2dba110
EviSong <evisong@gmail.com>
2010-12-12 10:30:48 +0000

    Updated data.

--- InstantNoodlesMaster/src/me/evis/mobile/noodle/provider/NoodlesContentProvider.java
+++ InstantNoodlesMaster/src/me/evis/mobile/noodle/provider/NoodlesContentProvider.java
@@ -35,10 +35,13 @@ public class NoodlesContentProvider extends ContentProvider {
 
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
             + "/" + TABLE_NAME);
-    public static final Uri ID_FIELD_CONTENT_URI = Uri.parse("content://"
-            + AUTHORITY + "/" + TABLE_NAME.toLowerCase() + "/id");
-    public static final Uri BRAND_ID_FIELD_CONTENT_URI = Uri.parse("content://"
-            + AUTHORITY + "/" + TABLE_NAME.toLowerCase() + "/brand_id");
+    public static final Uri _ID_FIELD_CONTENT_URI = Uri.parse("content://"
+            + AUTHORITY + "/" + TABLE_NAME.toLowerCase());
+    public static final Uri UUID_FIELD_CONTENT_URI = Uri.parse("content://"
+            + AUTHORITY + "/" + TABLE_NAME.toLowerCase() + "/uuid");
+    public static final Uri BRAND_UUID_FIELD_CONTENT_URI = Uri
+            .parse("content://" + AUTHORITY + "/" + TABLE_NAME.toLowerCase()
+                    + "/brand_uuid");
     public static final Uri NAME_FIELD_CONTENT_URI = Uri.parse("content://"
             + AUTHORITY + "/" + TABLE_NAME.toLowerCase() + "/name");
     public static final Uri SOAKAGE_TIME_FIELD_CONTENT_URI = Uri
@@ -48,12 +51,12 @@ public class NoodlesContentProvider extends ContentProvider {
             + AUTHORITY + "/" + BARCODE_TABLE_NAME.toLowerCase() + "/code");
     
     /**
-     * The MIME type of {@link #CONTENT_URI} providing a directory of notes.
+     * The MIME type of {@link #CONTENT_URI} providing a directory of noodles.
      */
     public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.me.evis.mobile.noodle.provider.noodles";
 
     /**
-     * The MIME type of a {@link #CONTENT_URI} sub-directory of a single note.
+     * The MIME type of a {@link #CONTENT_URI} sub-directory of a single noodle.
      */
     public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.me.evis.mobile.noodle.provider.noodles";
 
@@ -62,22 +65,24 @@ public class NoodlesContentProvider extends ContentProvider {
     private static final UriMatcher URL_MATCHER;
 
     private static final int NOODLES = 1;
-    private static final int NOODLES_ID = 2;
-    private static final int NOODLES_BRAND_ID = 3;
-    private static final int NOODLES_NAME = 4;
-    private static final int NOODLES_SOAKAGE_TIME = 5;
-    private static final int BARCODE_CODE = 6;
+    private static final int NOODLES__ID = 2;
+    private static final int NOODLES_UUID = 3;
+    private static final int NOODLES_BRAND_UUID = 4;
+    private static final int NOODLES_NAME = 5;
+    private static final int NOODLES_SOAKAGE_TIME = 6;
+    private static final int BARCODE_CODE = 7;
 
     // Content values keys (using column names)
     public static final String _ID = "_id";
-    public static final String BRAND_ID = "brand_id";
+    public static final String UUID = "uuid";
+    public static final String BRAND_UUID = "brand_uuid";
     public static final String NAME = "name";
     public static final String NET_WEIGHT = "net_weight";
     public static final String NOODLES_WEIGHT = "noodles_weight";
-    public static final String STEP_1_ID = "step_1_id";
-    public static final String STEP_2_ID = "step_2_id";
-    public static final String STEP_3_ID = "step_3_id";
-    public static final String STEP_4_ID = "step_4_id";
+    public static final String STEP_1_UUID = "step_1_uuid";
+    public static final String STEP_2_UUID = "step_2_uuid";
+    public static final String STEP_3_UUID = "step_3_uuid";
+    public static final String STEP_4_UUID = "step_4_uuid";
     public static final String SOAKAGE_TIME = "soakage_time";
     public static final String DESCRIPTION = "description";
     public static final String LOGO = "logo";
@@ -98,20 +103,24 @@ public class NoodlesContentProvider extends ContentProvider {
             qb.setTables(TABLE_NAME);
             qb.setProjectionMap(NOODLES_PROJECTION_MAP);
             break;
-        case NOODLES_ID:
+        case NOODLES__ID:
             qb.setTables(TABLE_NAME
-            		+ " LEFT JOIN " + BRAND_TABLE_NAME + " ON " + TABLE_NAME + "." + BRAND_ID + "=" + BRAND_TABLE_NAME + "._id"
-            		+ " LEFT JOIN " + MANUFACTURER_TABLE_NAME + " ON " + BRAND_TABLE_NAME + ".manufacturer_id=" + MANUFACTURER_TABLE_NAME + "._id"
-            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step1 ON " + TABLE_NAME + "." + STEP_1_ID + "=" + "step1._id"
-            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step2 ON " + TABLE_NAME + "." + STEP_2_ID + "=" + "step2._id"
-            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step3 ON " + TABLE_NAME + "." + STEP_3_ID + "=" + "step3._id"
-            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step4 ON " + TABLE_NAME + "." + STEP_4_ID + "=" + "step4._id"
+            		+ " LEFT JOIN " + BRAND_TABLE_NAME + " ON " + TABLE_NAME + "." + BRAND_UUID + "=" + BRAND_TABLE_NAME + ".uuid"
+            		+ " LEFT JOIN " + MANUFACTURER_TABLE_NAME + " ON " + BRAND_TABLE_NAME + ".manufacturer_uuid=" + MANUFACTURER_TABLE_NAME + ".uuid"
+            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step1 ON " + TABLE_NAME + "." + STEP_1_UUID + "=" + "step1.uuid"
+            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step2 ON " + TABLE_NAME + "." + STEP_2_UUID + "=" + "step2.uuid"
+            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step3 ON " + TABLE_NAME + "." + STEP_3_UUID + "=" + "step3.uuid"
+            		+ " LEFT JOIN " + STEP_TABLE_NAME + " step4 ON " + TABLE_NAME + "." + STEP_4_UUID + "=" + "step4.uuid"
             	);
-            qb.appendWhere(TABLE_NAME+ "._id='" + url.getPathSegments().get(2) + "'");
+            qb.appendWhere(TABLE_NAME+ "._id=" + url.getPathSegments().get(1));
             break;
-        case NOODLES_BRAND_ID:
+        case NOODLES_UUID:
             qb.setTables(TABLE_NAME);
-            qb.appendWhere("brand_id='" + url.getPathSegments().get(2) + "'");
+            qb.appendWhere("uuid='" + url.getPathSegments().get(2) + "'");
+            break;
+        case NOODLES_BRAND_UUID:
+            qb.setTables(TABLE_NAME);
+            qb.appendWhere("brand_uuid='" + url.getPathSegments().get(2) + "'");
             break;
         case NOODLES_NAME:
             qb.setTables(TABLE_NAME);
@@ -126,8 +135,8 @@ public class NoodlesContentProvider extends ContentProvider {
         case BARCODE_CODE:
             qb.setTables(TABLE_NAME
             		+ " JOIN " + BARCODE_TABLE_NAME + " ON " + TABLE_NAME + "._id=" + BARCODE_TABLE_NAME + ".noodles_id"
-            		+ " LEFT JOIN " + BRAND_TABLE_NAME + " ON " + TABLE_NAME + "." + BRAND_ID + "=" + BRAND_TABLE_NAME + "._id"
-            		+ " LEFT JOIN " + MANUFACTURER_TABLE_NAME + " ON " + BRAND_TABLE_NAME + ".manufacturer_id=" + MANUFACTURER_TABLE_NAME + "._id"
+            		+ " LEFT JOIN " + BRAND_TABLE_NAME + " ON " + TABLE_NAME + "." + BRAND_UUID + "=" + BRAND_TABLE_NAME + ".uuid"
+            		+ " LEFT JOIN " + MANUFACTURER_TABLE_NAME + " ON " + BRAND_TABLE_NAME + ".manufacturer_uuid=" + MANUFACTURER_TABLE_NAME + ".uuid"
 //            		+ " LEFT JOIN " + STEP_TABLE_NAME + " AS step1 ON " + TABLE_NAME + "." + STEP_1_ID + "=" + "step1._id"
 //            		+ " LEFT JOIN " + STEP_TABLE_NAME + " AS step2 ON " + TABLE_NAME + "." + STEP_2_ID + "=" + "step2._id"
 //            		+ " LEFT JOIN " + STEP_TABLE_NAME + " AS step3 ON " + TABLE_NAME + "." + STEP_3_ID + "=" + "step3._id"
@@ -156,9 +165,11 @@ public class NoodlesContentProvider extends ContentProvider {
         switch (URL_MATCHER.match(url)) {
         case NOODLES:
             return CONTENT_TYPE;
-        case NOODLES_ID:
+        case NOODLES__ID:
             return CONTENT_ITEM_TYPE;
-        case NOODLES_BRAND_ID:
+        case NOODLES_UUID:
+            return CONTENT_ITEM_TYPE;
+        case NOODLES_BRAND_UUID:
             return CONTENT_TYPE;
         case NOODLES_NAME:
             // A fuzzy inquiry.
@@ -205,10 +216,26 @@ public class NoodlesContentProvider extends ContentProvider {
         case NOODLES:
             count = mDB.delete(TABLE_NAME, where, whereArgs);
             break;
-        case NOODLES_ID:
-            segment = "'" + url.getPathSegments().get(2) + "'";
+        case NOODLES__ID:
+            segment = url.getPathSegments().get(1);
             count = mDB.delete(TABLE_NAME,
                     "_id="
+                            + segment
+                            + (!TextUtils.isEmpty(where) ? " AND (" + where
+                                    + ')' : ""), whereArgs);
+            break;
+        case NOODLES_UUID:
+            segment = "'" + url.getPathSegments().get(2) + "'";
+            count = mDB.delete(TABLE_NAME,
+                    "uuid="
+                            + segment
+                            + (!TextUtils.isEmpty(where) ? " AND (" + where
+                                    + ')' : ""), whereArgs);
+            break;
+        case NOODLES_BRAND_UUID:
+            segment = "'" + url.getPathSegments().get(2) + "'";
+            count = mDB.delete(TABLE_NAME,
+                    "brand_uuid="
                             + segment
                             + (!TextUtils.isEmpty(where) ? " AND (" + where
                                     + ')' : ""), whereArgs);
@@ -231,10 +258,26 @@ public class NoodlesContentProvider extends ContentProvider {
         case NOODLES:
             count = mDB.update(TABLE_NAME, values, where, whereArgs);
             break;
-        case NOODLES_ID:
-            segment = "'" + url.getPathSegments().get(2) + "'";
+        case NOODLES__ID:
+            segment = url.getPathSegments().get(1);
             count = mDB.update(TABLE_NAME, values,
                     "_id="
+                            + segment
+                            + (!TextUtils.isEmpty(where) ? " AND (" + where
+                                    + ')' : ""), whereArgs);
+            break;
+        case NOODLES_UUID:
+            segment = "'" + url.getPathSegments().get(2) + "'";
+            count = mDB.update(TABLE_NAME, values,
+                    "uuid="
+                            + segment
+                            + (!TextUtils.isEmpty(where) ? " AND (" + where
+                                    + ')' : ""), whereArgs);
+            break;
+        case NOODLES_BRAND_UUID:
+            segment = "'" + url.getPathSegments().get(2) + "'";
+            count = mDB.update(TABLE_NAME, values,
+                    "brand_uuid="
                             + segment
                             + (!TextUtils.isEmpty(where) ? " AND (" + where
                                     + ')' : ""), whereArgs);
@@ -250,10 +293,12 @@ public class NoodlesContentProvider extends ContentProvider {
     static {
         URL_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
         URL_MATCHER.addURI(AUTHORITY, TABLE_NAME.toLowerCase(), NOODLES);
-        URL_MATCHER.addURI(AUTHORITY, TABLE_NAME.toLowerCase() + "/id" + "/*",
-                NOODLES_ID);
-        URL_MATCHER.addURI(AUTHORITY, TABLE_NAME.toLowerCase() + "/brand_id"
-                + "/*", NOODLES_BRAND_ID);
+        URL_MATCHER.addURI(AUTHORITY, TABLE_NAME.toLowerCase() + "/#",
+                NOODLES__ID);
+        URL_MATCHER.addURI(AUTHORITY,
+                TABLE_NAME.toLowerCase() + "/uuid" + "/*", NOODLES_UUID);
+        URL_MATCHER.addURI(AUTHORITY, TABLE_NAME.toLowerCase() + "/brand_uuid"
+                + "/*", NOODLES_BRAND_UUID);
         URL_MATCHER.addURI(AUTHORITY,
                 TABLE_NAME.toLowerCase() + "/name" + "/*", NOODLES_NAME);
         URL_MATCHER.addURI(AUTHORITY, TABLE_NAME.toLowerCase()
@@ -263,14 +308,15 @@ public class NoodlesContentProvider extends ContentProvider {
 
         NOODLES_PROJECTION_MAP = new HashMap<String, String>();
         NOODLES_PROJECTION_MAP.put(_ID, "_id");
-        NOODLES_PROJECTION_MAP.put(BRAND_ID, "brand_id");
+        NOODLES_PROJECTION_MAP.put(UUID, "uuid");
+        NOODLES_PROJECTION_MAP.put(BRAND_UUID, "brand_uuid");
         NOODLES_PROJECTION_MAP.put(NAME, "name");
         NOODLES_PROJECTION_MAP.put(NET_WEIGHT, "net_weight");
         NOODLES_PROJECTION_MAP.put(NOODLES_WEIGHT, "noodles_weight");
-        NOODLES_PROJECTION_MAP.put(STEP_1_ID, "step_1_id");
-        NOODLES_PROJECTION_MAP.put(STEP_2_ID, "step_2_id");
-        NOODLES_PROJECTION_MAP.put(STEP_3_ID, "step_3_id");
-        NOODLES_PROJECTION_MAP.put(STEP_4_ID, "step_4_id");
+        NOODLES_PROJECTION_MAP.put(STEP_1_UUID, "step_1_uuid");
+        NOODLES_PROJECTION_MAP.put(STEP_2_UUID, "step_2_uuid");
+        NOODLES_PROJECTION_MAP.put(STEP_3_UUID, "step_3_uuid");
+        NOODLES_PROJECTION_MAP.put(STEP_4_UUID, "step_4_uuid");
         NOODLES_PROJECTION_MAP.put(SOAKAGE_TIME, "soakage_time");
         NOODLES_PROJECTION_MAP.put(DESCRIPTION, "description");
         NOODLES_PROJECTION_MAP.put(LOGO, "logo");
