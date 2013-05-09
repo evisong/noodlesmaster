0467e1d149608dacab19a72b4caf941ba2a29e0a
EviSong <evisong@gmail.com>
2013-05-09 15:13:49 +0000

    Added some logs; changed main activity to single instance; refined notification icon; refined appwidget based on google's app_widget_templates-v4.0.

--- src/me/evis/mobile/noodle/NoodlesTimerAlarmer.java
+++ src/me/evis/mobile/noodle/NoodlesTimerAlarmer.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.widget.Toast;
 
 public class NoodlesTimerAlarmer extends BroadcastReceiver {
@@ -42,30 +41,19 @@ public class NoodlesTimerAlarmer extends BroadcastReceiver {
 	private void sendNotification(Context context) {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.noodles_master_logo)
+                .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle(context.getString(R.string.notification_timer_complete_title))
                 .setContentText(context.getString(R.string.notification_timer_complete_text))
                 .setSound(soundUri)
                 .setAutoCancel(true);
+        
         // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(context, NoodlesMaster.class);
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.from(context);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(NoodlesMaster.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                    0,
-                    PendingIntent.FLAG_UPDATE_CURRENT
-                );
+        PendingIntent resultPendingIntent = 
+                PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(resultPendingIntent);
+        
         NotificationManager mNotificationManager =
             (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         // mId allows you to update the notification later on.
