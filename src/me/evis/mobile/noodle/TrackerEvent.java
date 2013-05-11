d7ecbbe1b643ae192fea67eaf535c94e1ccac6a2
EviSong <evisong@gmail.com>
2013-05-11 08:36:30 +0000

    GA tracks the first run after installation.

--- src/me/evis/mobile/noodle/TrackerEvent.java
+++ src/me/evis/mobile/noodle/TrackerEvent.java
@@ -4,4 +4,7 @@ public class TrackerEvent {
     public static final String CATEGORY_UI = "ui";
     public static final String ACTION_BUTTON = "button";
     public static final String ACTION_WIDGET = "widget";
+    
+    public static final String CATEGORY_APP = "app";
+    public static final String ACTION_REGISTER = "register";
 }
