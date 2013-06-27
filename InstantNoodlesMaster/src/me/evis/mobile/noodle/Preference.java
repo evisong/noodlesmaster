48acf4afb85b989f76a6647c6515b8062bc6e603
EviSong <evisong@gmail.com>
2013-06-27 01:35:33 +0000

    Bugfix: buttons with wrong links on About view.

--- InstantNoodlesMaster/src/me/evis/mobile/noodle/Preference.java
+++ InstantNoodlesMaster/src/me/evis/mobile/noodle/Preference.java
@@ -14,11 +14,11 @@
 package me.evis.mobile.noodle;
 
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-
-import me.evis.mobile.noodle.R;
+import android.util.Log;
 
 /***
  * PreferenceActivity is a built-in Activity for preferences management
@@ -32,6 +32,7 @@ import me.evis.mobile.noodle.R;
  * Type>("<Preference Key>",<default value>);
  */
 public class Preference extends PreferenceActivity {
+    private static final String TAG = "Preference";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,5 +42,12 @@ public class Preference extends PreferenceActivity {
         PreferenceScreen psAbout = (PreferenceScreen) getPreferenceScreen().findPreference("about");
         Intent aboutIntent = new Intent(this, About.class);
         psAbout.setIntent(aboutIntent);
+        try {
+            String appVer = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+            psAbout.setSummary(getString(R.string.about_version_label) + appVer);
+        }
+        catch (NameNotFoundException e) {
+            Log.v(TAG, e.getMessage());
+        }
     }
 }
